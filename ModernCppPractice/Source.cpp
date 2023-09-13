eb1ef99f994083225dc611f0fe94b1b3cb468d98
LutLas <lutakachihota@gmail.com>
2023-09-13 09:45:52 +0200

    Update Source.cpp

--- ModernCppPractice/Source.cpp
+++ ModernCppPractice/Source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+class MyClass
+{
+public:
+	static int x; // declare a static data member
+	static void myfunction(); // declare a static member function
+};
+
+int MyClass::x = 123; // define a static data member
+// define a static member function
+void MyClass::myfunction()
+{
+	std::cout << "Hello World from a static member function.";
+}
+
 int main()
 {
-	std::cout << "Hello World.";
+	MyClass::x = 456; // access a static data member
+	std::cout << "Static data member value is: " << MyClass::x;
+	MyClass::myfunction(); // call a static member function
 }
